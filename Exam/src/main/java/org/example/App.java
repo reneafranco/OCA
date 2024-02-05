87eff461c7e5669748683515cbadedeeae66b368
Rene Franco <reneadonayfrancoperez@gmail.com>
2024-02-05 12:17:48 -0500

    update-quickStart methdos

--- Exam/src/main/java/org/example/App.java
+++ Exam/src/main/java/org/example/App.java
@@ -6,8 +6,11 @@ package org.example;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+
+
     }
+
+
+
 }
---
+++ Exam/src/main/java/org/example/methos/Methdos.java
@@ -0,0 +1,4 @@
+package org.example.methos;
+
+public class Methdos {
+}
