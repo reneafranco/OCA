efbabeddca3158e7b01af3fc969ae3db43aa2d82
Rene Franco <reneadonayfrancoperez@gmail.com>
2024-02-05 12:23:03 -0500

    update-quickStart methdos

--- Exam/src/main/java/org/example/methos/Methdos.java
+++ Exam/src/main/java/org/example/methos/Methdos.java
@@ -1,4 +1,18 @@
 package org.example.methos;
 
 public class Methdos {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+        change(x);
+        System.out.println(x);
+        //pass by value mechanism
+        // primitives data type pass the value not the pointer or the reference
+    }
+
+    static void change(int num){
+        num = 10;
+    }
+
 }
