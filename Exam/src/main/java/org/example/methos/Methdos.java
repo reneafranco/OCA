88e88982754242c9634a6a3ff8a669d2de579765
Rene Franco <reneadonayfrancoperez@gmail.com>
2024-02-05 12:30:45 -0500

    update-quickStart methdos

--- Exam/src/main/java/org/example/methos/Methdos.java
+++ Exam/src/main/java/org/example/methos/Methdos.java
@@ -9,10 +9,34 @@ public class Methdos {
         System.out.println(x);
         //pass by value mechanism
         // primitives data type pass the value not the pointer or the reference
+
+
+        String  webby = "Webyy";
+        stringChange(webby);
+        webby = stringChangeReturn(webby);
+        System.out.println(webby);
+
+        StringBuilder strbuilder = new StringBuilder("Hello ");
+        changeStrBuilder(strbuilder);
+        System.out.println(strbuilder);
+
     }
 
     static void change(int num){
         num = 10;
+    }
+
+    static void stringChange(String webby){
+        webby = "bird";
+    }
+
+    static String stringChangeReturn(String webby){
+         webby = "bird";
+        return webby;
+    }
+    static void changeStrBuilder(StringBuilder builder){
+        builder.append("hehehehe");
+
     }
 
 }
